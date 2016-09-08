878f2bcb5c476e7c7ac2ce6c8f7719a38772363b
Adam Roben <adam@roben.org>
2016-09-08 15:26:44 -0400

    Get RSpec working?
    @world.announce_filters just prints "No examples found".
    
    Not at all sure that group splitting is working. We need a test.

--- lib/test_queue/runner/rspec.rb
+++ lib/test_queue/runner/rspec.rb
@@ -20,33 +20,71 @@ module TestQueue
   class Runner
     class RSpec < Runner
       def initialize
-        @rspec = ::RSpec::Core::QueueRunner.new
-        @split_groups = ENV['TEST_QUEUE_SPLIT_GROUPS'] && ENV['TEST_QUEUE_SPLIT_GROUPS'].strip.downcase == 'true'
-        if @split_groups
-          groups = @rspec.example_groups
-          groups_to_split, groups_to_keep = [], []
-          groups.each do |group|
-            (group.metadata[:no_split] ? groups_to_keep : groups_to_split) << group
-          end
-          queue = groups_to_split.map(&:descendant_filtered_examples).flatten
-          queue.concat groups_to_keep
-          queue.sort_by!{ |s| -(stats[s.id] || 0) }
-        else
-          queue = @rspec.example_groups.sort_by{ |s| -(stats[s.to_s] || 0) }
-        end
-
-        super(queue)
+        super([])
       end
 
 
       def run_worker(iterator)
-        @rspec.run_each(iterator).to_i
+        rspec = ::RSpec::Core::QueueRunner.new
+        rspec.run_each(iterator).to_i
       end
 
       def summarize_worker(worker)
         worker.summary  = worker.lines.grep(/ examples?, /).first
         worker.failure_output = worker.output[/^Failures:\n\n(.*)\n^Finished/m, 1]
       end
+    end
+  end
+
+  class TestFramework
+    def discover_suites
+      options = RSpec::Core::ConfigurationOptions.new(ARGV)
+      options.parse_options if options.respond_to?(:parse_options)
+      options.configure(RSpec.configuration)
+
+      RSpec.configuration.files_to_run.uniq.each do |file|
+        RSpec.world.reset
+        load file
+        split_groups(RSpec.world.example_groups).each do |example_or_group|
+          yield example_or_group.to_s, file
+        end
+      end
+    end
+
+    def load_suite(suite_name, path)
+      @suites ||= {}
+
+      suite = @suites[suite_name]
+      return suite if suite
+
+      RSpec.world.reset
+      begin
+        load path
+      rescue LoadError
+        return nil
+      end
+      split_groups(RSpec.world.example_groups).each do |example_or_group|
+        @suites[example_or_group.to_s] = example_or_group
+      end
+      @suites[suite_name]
+    end
+
+    def split_groups(groups)
+      return groups unless split_groups?
+
+      # FIXME: Need a test for this.
+      groups_to_split, groups_to_keep = [], []
+      groups.each do |group|
+        (group.metadata[:no_split] ? groups_to_keep : groups_to_split) << group
+      end
+      queue = groups_to_split.flat_map(&:descendant_filtered_examples)
+      queue.concat groups_to_keep
+      queue
+    end
+
+    def split_groups?
+      return @split_groups if defined?(@split_groups)
+      @split_groups = ENV['TEST_QUEUE_SPLIT_GROUPS'] && ENV['TEST_QUEUE_SPLIT_GROUPS'].strip.downcase == 'true'
     end
   end
 end
--- lib/test_queue/test_framework.rb
+++ lib/test_queue/test_framework.rb
@@ -1,42 +1,45 @@
-# This class provides an abstraction over the various test frameworks we
-# support. The framework-specific implementations are defined in the various
-# test_queue/runner/* files. This file just defines the interface.
-class TestFramework
-  # Discover all suites to run by loading them from disk.
-  #
-  # An example implementation might `require` test files from the repository
-  # one-by-one and yield the suites found in each file. This is called in a
-  # separate process; changes to global state will not affect the master or its
-  # workers.
-  #
-  # Yields a series of 2-element Arrays containing:
-  #
-  # suite_name - String suite name (or could be an RSpec Example/ExampleGroup
-  #              ID; whatever's appropriate for the test framework)
-  # path       - String file path of the file that contains the suite
-  def discover_suites
-  end
+module TestQueue
+  # This class provides an abstraction over the various test frameworks we
+  # support. The framework-specific implementations are defined in the various
+  # test_queue/runner/* files. This file just defines the interface.
+  class TestFramework
+    # Discover all suites to run by loading them from disk.
+    #
+    # An example implementation might `require` test files from the repository
+    # one-by-one and yield the suites found in each file. This is called in a
+    # separate process; changes to global state will not affect the master or its
+    # workers.
+    #
+    # Yields a series of 2-element Arrays containing:
+    #
+    # suite_name - String suite name (or could be an RSpec Example/ExampleGroup
+    #              ID; whatever's appropriate for the test framework)
+    # path       - String file path of the file that contains the suite
+    def discover_suites
+      raise NotImplementedError
+    end
 
-  # Load the specified suite from the specified path.
-  #
-  # suite_name - String suite name (or could be an RSpec Example/ExampleGroup
-  #              ID; whatever's appropriate for the test framework)
-  # path       - String file path of the file that contains the suite
-  #
-  # Returns a runnable test suite object appropriate for the test framework.
-  def load_suite(suite_name, path)
-    raise NotImplementedError
-  end
+    # Load the specified suite from the specified path.
+    #
+    # suite_name - String suite name (or could be an RSpec Example/ExampleGroup
+    #              ID; whatever's appropriate for the test framework)
+    # path       - String file path of the file that contains the suite
+    #
+    # Returns a runnable test suite object appropriate for the test framework.
+    def load_suite(suite_name, path)
+      raise NotImplementedError
+    end
 
-  # Filter the list of suites to be run.
-  #
-  # For instance, some test frameworks might support limiting the suites to be
-  # run based on command line parameters.
-  #
-  # suites - Array of TestQueue::Stats::Suite pulled from TestQueue::Stats.
-  #
-  # Returns an Array of TestQueue::Stats::Suite after applying filtering.
-  def filter_suites(suites)
-    suites
+    # Filter the list of suites to be run.
+    #
+    # For instance, some test frameworks might support limiting the suites to be
+    # run based on command line parameters.
+    #
+    # suites - Array of TestQueue::Stats::Suite pulled from TestQueue::Stats.
+    #
+    # Returns an Array of TestQueue::Stats::Suite after applying filtering.
+    def filter_suites(suites)
+      suites
+    end
   end
 end
