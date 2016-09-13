62d08ee1bc395b22dbb56644f2c17f742fdce4bb
Adam Roben <adam@roben.org>
2016-09-13 10:43:10 -0400

    Minitest 5 support

--- lib/test_queue/runner/minitest5.rb
+++ lib/test_queue/runner/minitest5.rb
@@ -3,9 +3,6 @@ require 'test_queue/runner'
 module MiniTest
   def self.__run reporter, options
     suites = Runnable.runnables
-
-    # Run the serial tests first after they complete, run the parallels tests
-    # We already sort suites based on its test_order at TestQueue::Runner::Minitest#initialize.
     suites.map { |suite| suite.run reporter, options }
   end
 
@@ -19,6 +16,16 @@ module MiniTest
     def self.runnables= runnables
       @@runnables = runnables
     end
+
+    # Synchronize all tests, even serial ones.
+    #
+    # Minitest runs serial tests before parallel ones to ensure the
+    # unsynchronized serial tests don't overlap the parallel tests. But since
+    # the test-queue master hands out tests without actually loading their
+    # code, there's no way to know which are parallel and which are serial.
+    # Synchronizing serial tests does add some overhead, but hopefully this is
+    # outweighed by the speed benefits of using test-queue.
+    def _synchronize; Test.io_lock.synchronize { yield }; end
   end
 
   class ProgressReporter
@@ -49,20 +56,56 @@ module TestQueue
   class Runner
     class MiniTest < Runner
       def initialize
-        tests = ::MiniTest::Test.runnables.reject { |s|
-          s.runnable_methods.empty?
-        }.sort_by { |s|
-          -(stats[s.to_s] || 0)
-        }.partition { |s|
-          s.test_order == :parallel
-        }.reverse.flatten
-        super(tests)
+        if ::MiniTest::Test.runnables.any? { |r| r.runnable_methods.any? }
+          fail "Do not `require` test files. Pass them via ARGV instead and they will be required as needed."
+        end
+        super([])
       end
 
       def run_worker(iterator)
         ::MiniTest::Test.runnables = iterator
         ::MiniTest.run ? 0 : 1
       end
+    end
+  end
+
+  class TestFramework
+    def discover_suites
+      ARGV.each do |arg|
+        ::MiniTest::Test.reset
+        require File.absolute_path(arg)
+        ::MiniTest::Test.runnables.
+          reject { |s| s.runnable_methods.empty? }.
+          each do |s|
+            yield s.name, arg
+          end
+      end
+    end
+
+    def load_suite(suite_name, path)
+      @suites ||= {}
+
+      suite = @suites[suite_name]
+      return suite if suite
+
+      ::MiniTest::Test.reset
+      begin
+        require File.absolute_path(path)
+      rescue LoadError
+        return nil
+      end
+      ::MiniTest::Test.runnables.
+        reject { |s| s.runnable_methods.empty? }.
+        each do |s|
+          @suites[s.name] = s
+        end
+
+      @suites[suite_name]
+    end
+
+    def filter_suites(suites)
+      paths = ARGV.to_set
+      suites.select { |suite| paths.include?(suite.path) }
     end
   end
 end
